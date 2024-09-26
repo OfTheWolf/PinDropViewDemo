73f3bb8e893d3c75b3497e3ca20c5a146534ace9
Ugur Bozkurt <uguboz@gmail.com>
2024-09-26 22:38:50 +0300

    Add pin drop view demo

--- PinDropViewDemo/ViewController.swift
+++ PinDropViewDemo/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import PinDropView
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var pinDropView: PinDropView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        pinDropView.borderColor = .red
+        pinDropView.borderWidth = 10
+        pinDropView.fillColor = .white
+        pinDropView.icon = .init(systemName: "carrot")!
+        pinDropView.iconColor = .orange
+        pinDropView.didTapHandler = {
+            print("did tap pin")
+        }
     }
 
 
